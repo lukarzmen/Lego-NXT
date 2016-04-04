4c7a0c9d87e1bfa4d49cf6a63d57325279afda79
lukarzmen <lukarzmen@gmail.com>
2016-04-04 20:52:56 +0200

    motor control
    motor control. controllers to overvrite

---
+++ SterowanieSilnikiem/MotorControllers.java
@@ -0,0 +1,27 @@
+public abstract class MotorControllers
+{
+	public void PID()
+	{
+		
+	}
+	
+	public void PI()
+	{
+		
+	}
+	
+	public void PD()
+	{
+		
+	}
+	
+	public void neuroController()
+	{
+		
+	}
+	
+	public void fuzzyController()
+	{
+		
+	}
+}
